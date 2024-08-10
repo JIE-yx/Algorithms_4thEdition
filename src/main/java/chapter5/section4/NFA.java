8c2f8b00be6ff19e12819b45e573ebbb715dd3a1
ada626 <ada.yxin@gmail.com>
2024-08-10 17:26:36 +0800

    政资正则匹配

--- src/main/java/chapter5/section4/NFA.java
+++ src/main/java/chapter5/section4/NFA.java
@@ -2,6 +2,7 @@ package chapter5.section4;
 
 import chapter4.section2.DirectGraph;
 import chapter4.section2.DirectedDFS;
+import com.google.gson.Gson;
 
 import java.util.*;
 
@@ -26,36 +27,56 @@ public class NFA {
     private char[] re;
 
 
-    public NFA(String regexp) {
-        re = regexp.toCharArray();
-        M = re.length;
+    public NFA(String regrep) {
+        re = regrep.toCharArray();
+        int M = re.length;
         G = new DirectGraph(M + 1);
-        Stack<Integer> ops = new Stack<Integer>();
-        for (int i = 0; i < M; i++) {
+        Stack<Integer> ops = new Stack<>();
+        for (int i = 0 ; i < M; i ++) {
+            char currentChar = re[i];
             int lp = i;
-            if (re[i] == '(' || re[i] == '|') {
+            /**
+             * 左括号、或符号，压入栈
+             */
+            if (currentChar == '|' || currentChar == '(') {
                 ops.push(i);
-            } else if (re[i] == ')') {
+            } else if (currentChar == ')') {
+                /**
+                 * 遇到右括号，需要构建(AB|CD) 形式的【空字符转换】边
+                 */
                 int or = ops.pop();
                 if (re[or] == '|') {
                     lp = ops.pop();
-                    G.addEdge(lp, or+1);
+                    G.addEdge(lp, or + 1);
                     G.addEdge(or, i);
                 } else {
+                    /**
+                     * 如果不是 （AB|CD）形式而是 （AB）形式，更新一下lp对应的状态
+                     * lp指向对应的左括号
+                     */
                     lp = or;
                 }
             }
-            if (i < M-1 && re[i+1] == '*') {
-                // 查看下一个字符
-                G.addEdge(lp, i+1);
-                G.addEdge(i+1, lp);
+            /**
+             * 处理*号
+             */
+            if (i < M - 1 && re[i+1] == '*') {
+                /**
+                 * 星号*需要和前面的普通字符，或者前面的左括号，进行双向指向
+                 * 例如 A*，需要A和*互相指向
+                 *      （ABC）*，需要（和*相互指向
+                 */
+                G.addEdge(lp, i + 1);
+                G.addEdge(i + 1, lp);
             }
-            if (re[i] == '(' || re[i] == '*' || re[i] == ')') {
-                G.addEdge(i, i+1);
+            if (currentChar == '(' || currentChar == ')' || currentChar == '*') {
+                /**
+                 * *、（、）都是可以直接【空字符转换】到下一个状态的
+                 */
+                G.addEdge(i, i + 1);
             }
         }
     }
-
     public boolean recognize(String txt) {
         /**
          * 从状态0开始，通过 【空字符转换】可以到达的所有状态
@@ -68,43 +89,52 @@ public class NFA {
                 possibleStatus.add(i);
             }
         }
+        Gson gson = new Gson();
+        System.out.println("init pc " + gson.toJson(possibleStatus));
         for (int i = 0; i < txt.length(); i ++) {
+            char txtC = txt.charAt(i);
+            System.out.println("i = " + i + ", txtc = " + txtC);
             Set<Integer> match = new HashSet<>();
             for (int v : possibleStatus) {
+                if (v == M) {
+                    return true;
+                }
                 if (v < M) {
                     /**
                      * 当前在正则匹配(re)的NFA的状态v，且收到了txt中对应的字符
                      * 或者re自身当前字符就是通配符 '.'
+                     * 此时可以直接进入下一个状态
                      */
                     char c = re[v];
-                    if (c == '.' || c == txt.charAt(i)) {
+                    if (c == '.' || c == txtC) {
                         match.add(v + 1);
-                        if (v + 1 == M) {
-                            return true;
-                        }
                     }
                 }
             }
+            System.out.println("match = " + gson.toJson(match));
             possibleStatus = new HashSet<>();
             dfs = new DirectedDFS(G, match);
-            if (dfs.canGetTo(M)) {
-                return true;
-            }
             for (int v = 0; v < G.getPointNum(); v ++) {
                 if (dfs.canGetTo(v)) {
                     possibleStatus.add(v);
                 }
             }
-            System.out.println(1);
+            System.out.println("pc = " + gson.toJson(possibleStatus));
+        }
+        for (int v : possibleStatus) {
+            if (v == M) {
+                return true;
+            }
         }
         return false;
     }
 
 
     public static void main(String[] args) {
-        String regp = "((A*B|AC)D)";
+        //             0123456789
+        String regp = ".*((A*B|AC)D).*";
         NFA nfa = new NFA(regp);
         System.out.println(nfa.G.toString());
-        System.out.println(nfa.recognize("AACDA"));
+        System.out.println(nfa.recognize("asjkohfaksfghAAAAABACDasdlkfhjasdg"));
     }
 }
